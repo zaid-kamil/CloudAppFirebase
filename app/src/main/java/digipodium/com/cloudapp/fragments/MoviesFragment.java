ac6cda1bdebb9af6f95b83c6fa85a8af52ecfa36
zaid kamil <xaidmetamorphos@gmail.com>
2019-06-28 13:07:44 +0530

    slowpoke

--- app/src/main/java/digipodium/com/cloudapp/fragments/MoviesFragment.java
+++ app/src/main/java/digipodium/com/cloudapp/fragments/MoviesFragment.java
@@ -1,15 +1,18 @@
 package digipodium.com.cloudapp.fragments;
 
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
@@ -39,17 +42,20 @@ public class MoviesFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_movie, container, false);
-        RecyclerView movieView = view.findViewById(R.id.movieList);
+        final RecyclerView movieView = view.findViewById(R.id.movieList);
         FirebaseFirestore firestore = FirebaseFirestore.getInstance();
         firestore.collection("movies").get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
                 if (task.isSuccessful()) {
 
-                    List<String> movies = new ArrayList<>();
+                    List<MyMovie> movies = new ArrayList<>();
                     for (QueryDocumentSnapshot snapshot : task.getResult()) {
+                        MyMovie movie = snapshot.toObject(MyMovie.class);
+                        movies.add(movie);
                     }
-
+                    movieView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                    movieView.setAdapter(new MovieListAdapter(getActivity(),movies));
                 } else {
                     Exception exception = task.getException();
                     Toast.makeText(getContext(), exception.getMessage(), Toast.LENGTH_SHORT).show();
@@ -59,4 +65,47 @@ public class MoviesFragment extends Fragment {
         return view;
     }
 
+    private class MovieListAdapter extends RecyclerView.Adapter<MovieListAdapter.Holder> {
+
+        private final LayoutInflater inflater;
+        private final List<MyMovie> movieList;
+
+        public MovieListAdapter(Context context, List<MyMovie> movieList) {
+            inflater = LayoutInflater.from(context);
+            this.movieList = movieList;
+        }
+
+        @NonNull
+        @Override
+        public Holder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            View v=inflater.inflate(R.layout.movie_card,parent,false);
+            return new Holder(v);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull Holder holder, int position) {
+            MyMovie movie = movieList.get(position);
+            holder.textMovie.setText(movie.movie);
+            holder.textClick.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(getActivity(), "nothing", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        @Override
+        public int getItemCount() {
+            return movieList.size();
+        }
+
+        class Holder extends RecyclerView.ViewHolder{
+            TextView textMovie,textClick;
+            public Holder(@NonNull View itemView) {
+                super(itemView);
+                textClick= itemView.findViewById(R.id.textClick);
+                textMovie= itemView.findViewById(R.id.textMovie);
+            }
+        }
+    }
 }
